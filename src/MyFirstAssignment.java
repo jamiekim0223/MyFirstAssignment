c857e9018cd0f943b6da7030a62a5795a4089539
jamiekim0223 <jamiekim0223@gmail.com>
2022-09-01 09:35:43 -0500

    Dog ASCII Art is complete.

---
+++ src/MyFirstAssignment.java
@@ -0,0 +1,12 @@
+public class MyFirstAssignment {
+    public static void main(String[] args) {
+        System.out.println("   __");
+        System.out.println("o-''|\\_____/)");
+        System.out.println(" \\_/|_)     )");
+        System.out.println("    \\  __  /");
+        System.out.println("    (_/ (_/  ");
+
+
+    }
+
+}
