fae502ddbf9447626444a39c606ca7f798f956ca
jamiekim0223 <jamiekim0223@gmail.com>
2022-09-01 09:39:48 -0500

    Dog ASCII Art is complete

--- src/MyFirstAssignment.java
+++ src/MyFirstAssignment.java
@@ -1,7 +1,7 @@
 public class MyFirstAssignment {
     public static void main(String[] args) {
         System.out.println("   __");
-        System.out.println("o-''|\\_____/)");
+        System.out.println("O-''|\\_____/)");
         System.out.println(" \\_/|_)     )");
         System.out.println("    \\  __  /");
         System.out.println("    (_/ (_/  ");
